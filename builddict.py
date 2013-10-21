7e0355d52b3f76c72eb79fc45a55f08b1347ed1e
Jed Kundl <KundlJ@GMail.com>
2013-10-21 10:41:35 -0400

    Expanded for Audio use

--- builddict.py
+++ builddict.py
@@ -7,9 +7,15 @@ Created on Tue Sep 10 10:10:04 2013
 
 from os import listdir
 from os.path import isfile, join, isdir
-from processfile import process_file
+from processfile import process_file_gonogo, process_file_audio
 
-def build_dict(dirname):
+def build_dict(test_type, dirname):
+    if test_type == 'audio':
+       return build_dict_audio(dirname)
+    elif test_type == 'gonogo':
+        return build_dict_gonogo(dirname)
+
+def build_dict_gonogo(dirname):
     """ Builds a dictionary with data categorized by condition, subject
         session, and trial. """
 
@@ -43,7 +49,53 @@ def build_dict(dirname):
 
         
         for current in filenames:
-            processed = process_file(current)
+            processed = process_file_gonogo(current)
+            
+            subject = processed[0][0]
+            session = processed[0][1]
+            trial = processed[0][2]
+
+            info = processed[1]
+            
+            data[condition][subject][session].update({trial: info})
+            
+    return data, [num_subjects, num_sessions, num_trials]
+
+def build_dict_audio(dirname):
+    """ Builds a dictionary with data categorized by condition, subject
+        session, and trial. """
+
+    data = {}
+    dirnames = [x for x in listdir(dirname) if isdir(join(dirname, x))]
+    num_subjects = num_sessions = num_trials = 0
+    
+    for condition in dirnames:
+        filenames = [join(join(dirname, condition), x) for x in listdir(join(dirname, condition)) if isfile(join(join(dirname, condition), x))]
+        data.update({condition: {}})
+        
+        for filename in filenames:
+            info = [x.strip('.txt') for x in filename.split('_')]
+
+            subject = int(info[1][8:])
+            session = int(info[2][8:])
+            trial = int(info[3][5:])
+
+            if subject > num_subjects:
+                num_subjects = subject
+            if session > num_sessions:
+                num_sessions = session
+            if trial > num_trials:
+                num_trials = trial
+                
+        for subjects in range(1, num_subjects + 1):
+            data[condition].update({str(subjects): {}})
+            for sessions in range(1, num_sessions + 1):
+                data[condition][str(subjects)].update({str(sessions): {}})
+                
+
+        
+        for current in filenames:
+            processed = process_file_audio(current)
             
             subject = processed[0][0]
             session = processed[0][1]
--- makefile.py
+++ makefile.py
@@ -6,10 +6,20 @@ Created on Tue Sep 10 10:10:01 2013
 """
 
 from builddict import build_dict
+from sys import exit
 
-def makefile(savename, dirname):
+def makefile(test_type, savename, dirname):
+
+    if test_type == 'audio' or test_type == 'gonogo':
+        pass
+    else:
+        print 'Error: Usage: test-type <audio | gonogo> output-name dir-name'
+        exit(1)
+
+    savename_subject = savename[:savename.find('.')] + '-by_subject.csv'
+    savename_evaluation = savename[:savename.find('.')] + '-by_evaluation.csv'
     
-    data, numbers = build_dict(dirname)
+    data, numbers = build_dict(test_type, dirname)
     
     num_conditions = len(data.keys())
     num_subjects = numbers[0]
@@ -48,10 +58,10 @@ def makefile(savename, dirname):
         pre_num = post_num = num_trials / 2
    
     
-    with open(savename, 'w') as file_pointer:
+    with open(savename_subject, 'w') as file_pointer:
         for itr0 in range(len(data_array)):
             file_pointer.write('Condition: ' + conditions[itr0] + '\n\n')
-            for itr1 in range(len(data_array[itr0])):
+            for itr1 in range(len(data_array[itr0]) - num_subjects, len(data_array[itr0])):
                 file_pointer.write('Subject ' + str(itr1 + 1) + ':\n,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for count in range(pre_num):
@@ -98,42 +108,42 @@ def makefile(savename, dirname):
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][4]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Top Median,')
+                file_pointer.write('Worst Median,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][5]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Top Mean,')
+                file_pointer.write('Worst Mean,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][6]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Bottom Median,')
+                file_pointer.write('Best Median,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][7]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Bottom Mean,')
+                file_pointer.write('Best Mean,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][8]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Top Throughput,')
+                file_pointer.write('Worst Throughput,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
                             file_pointer.write(str(data_array[itr0][itr1][itr2][itr3][9]))
                         file_pointer.write(',')
                 file_pointer.write('\n')
-                file_pointer.write('Bottom Throughput,')
+                file_pointer.write('Best Throughput,')
                 for itr2 in range(len(data_array[itr0][itr1])):
                     for itr3 in range(len(data_array[itr0][itr1][itr2])):
                         if type(data_array[itr0][itr1][itr2][itr3]) is list:
@@ -141,3 +151,45 @@ def makefile(savename, dirname):
                         file_pointer.write(',')
                 file_pointer.write('\n\n')
             file_pointer.write('\n')
+
+    metrics = ['Valid Median', 'Valid Mean', 'Percent Missed', 'Percent False', 'Error Rate', \
+                'Top Median', 'Top Mean', 'Bottom Median', 'Bottom Mean', 'Top Throughput', 'Bottom Throughput' ]
+    metric_num = 0
+
+    with open(savename_evaluation, 'w') as file_pointer:
+        for itr0 in range(len(data_array)):
+            file_pointer.write('Condition: ' + conditions[itr0] + '\n\n')
+            for metric in metrics:
+                file_pointer.write(metric + ':\n,')
+                for itr1 in range(num_sessions):
+                    for count in range(pre_num):
+                        file_pointer.write(',')
+                    file_pointer.write('Session ' + str(itr1 + 1) + ',')
+                    for count in range(post_num):
+                        file_pointer.write(',')
+                file_pointer.write('\n,')
+                for itr1 in range(num_sessions):
+                    for itr2 in range(num_trials):
+                        file_pointer.write('Trial ' + str(itr2 + 1) + ',')
+                file_pointer.write('\n')
+                for itr2 in range(len(data_array[itr0]) - num_subjects, len(data_array[itr0])):
+                    file_pointer.write('Subject: ' + str(itr2 + 1) + ',')
+                    session_loc = trial_loc = 0
+                    for itr3 in range(num_sessions):
+                        for itr4 in range(num_trials):
+                            if type(data_array[itr0][itr2][session_loc][trial_loc]) is list:
+                                if metric_num == 2 or metric_num == 3:
+                                    file_pointer.write(str(data_array[itr0][itr2][session_loc][trial_loc][metric_num]) + '%')
+                                else:
+                                    file_pointer.write(str(data_array[itr0][itr2][session_loc][trial_loc][metric_num]))
+                            file_pointer.write(',')
+                            trial_loc += 1
+                        session_loc += 1
+                        trial_loc = 0
+                    file_pointer.write('\n')
+                metric_num += 1
+                file_pointer.write('\n')
+            metric_num = 0
+
+
+
